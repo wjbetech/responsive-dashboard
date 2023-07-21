a500266a8e90e910927edd4d1946bc776be96d40
wjbetech <wjbetech@gmail.com>
2023-07-21 15:00:33 +0900

    Added dark and light mode, cleaned up some CSS styling

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-// handle sideLink selection
-
-const sideLinks = document.querySelectorAll('.sidebar .side-menu-top li a');
-
-function handleClick(item) {
-    const li = item.parentElement;
-    sideLinks.forEach(i => {
-        i.parentElement.classList.remove('active');
-    });
-    li.classList.add('active');
-}
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
 
 sideLinks.forEach(item => {
-    item.removeEventListener('click', handleClick); // Remove any existing event listeners
-    item.addEventListener('click', () => handleClick(item));
+    const li = item.parentElement;
+    item.addEventListener('click', () => {
+        sideLinks.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
 });
-
-// handle sideBar toggle
 
 const menuBar = document.querySelector('.content nav .bx.bx-menu');
 const sideBar = document.querySelector('.sidebar');
@@ -24,21 +17,40 @@ menuBar.addEventListener('click', () => {
     sideBar.classList.toggle('close');
 });
 
-// // handle {...}
+const searchBtn = document.querySelector('.content nav form .form-input button');
+const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+const searchForm = document.querySelector('.content nav form');
 
-// const searchButton = document.querySelector('.content nav form .form-input button');
-// const searchButtonIcon = document.querySelector('.content nav form .form-input button .bx');
-// const searchForm = document.querySelector('.content nav form');
+searchBtn.addEventListener('click', function (e) {
+    if (window.innerWidth < 576) {
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+});
 
-// searchButton.addEventListener('click', function (event) {
-//     if (window.innerWidth < 576) {
-//         event.preventDefault;
-//         searchForm.classList.toggle('show');
-//         if (searchForm.classList.contains('show')) {
-//             searchButtonIcon.classList.remove('bx-search', 'bx-x');
-//         } else {
-//             searchButtonIcon.classList.replace('bx-x', 'bx-search');
-//         }
-//     }
-// })
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        sideBar.classList.add('close');
+    } else {
+        sideBar.classList.remove('close');
+    }
+    if (window.innerWidth > 576) {
+        searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+});
 
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
